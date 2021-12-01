ecb2e770b34c701895756947e97ead2a8b61bdd4
JULIAN122408 <julian.buritica4@hotmail.com>
2021-11-30 22:50:36 -0500

    cerrar sesion

--- front_2/src/components/navbar/navbar.js
+++ front_2/src/components/navbar/navbar.js
@@ -3,12 +3,21 @@ import {Navbar, Container, Nav, Dropdown, DropdownButton, Row } from 'react-boot
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import './navbar.css'
+import Cookies from 'universal-cookie/es6'
+
+const cookies = new Cookies();
 
 export default class Menu  extends React.Component {
     constructor(props) {
         super(props);
         this.state = {  }
     }
+
+    Logout(){
+        cookies.remove('_s');
+        window.location.reload();
+    }
+
     render() { 
         return ( 
                 <Navbar fixed="top" id="navbar" bg="primary" variant="dark">
@@ -31,7 +40,7 @@ export default class Menu  extends React.Component {
                     </Row>
                 </Dropdown.Header>
                 <Dropdown.Divider />
-                <Dropdown.Item href="/login">Cerrar Sesion</Dropdown.Item>
+                <Dropdown.Item onClick={() => this.Logout()}>Cerrar Sesion</Dropdown.Item>
                 {/*<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
                 <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>*/}
             </DropdownButton>
